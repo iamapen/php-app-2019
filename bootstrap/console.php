f75cb732b131e7dbc70bf0318d549a32eb487c7d
pen <iamapen@studiopoppy.com>
2020-02-27 23:45:47 +0900

    Add: バッチからバッチを呼ぶ例

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -9,13 +9,19 @@ require __DIR__ . '/../vendor/autoload.php';
 use Psr\Log\LogLevel;
 
 // 環境変数
+putenv(sprintf('APP_ROOT=%s', realpath(__DIR__ . '/..')));
+putenv(sprintf('TMP_DIR=%s', getenv('APP_ROOT') . '/storage/tmp'));
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
 $dotenv->required('LOG_LEVEL')->allowedValues([
     LogLevel::DEBUG, LogLevel::INFO, LogLevel::NOTICE,
     LogLevel::WARNING, LogLevel::ERROR,
     LogLevel::CRITICAL, LogLevel::ALERT, LogLevel::EMERGENCY,
 ]);
+$dotenv->required('APP_ROOT');
+$dotenv->required('TMP_DIR');
 
 $app = new Acme\App\Adapter\Console\Application(
     require __DIR__ . '/container.php'
@@ -24,5 +30,6 @@ $app->registerErrorHandler();
 
 // バッチコマンド登録
 $app->registerCommand(\Acme\App\Adapter\Console\Command\HelloCommand::class);
+$app->registerCommand(\Acme\App\Adapter\Console\Command\ChainCommand::class);
 
 return $app;
---
+++ src/App/Adapter/Console/Command/ChainCommand.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Acme\App\Adapter\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * バッチからバッチを呼ぶ例
+ */
+class ChainCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('app2019:chain')
+            ->setDescription('バッチからバッチを呼ぶ例');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helloCommand = new HelloCommand();
+        $helloInput = new ArrayInput(['outDir' => getenv('TMP_DIR')], $helloCommand->getDefinition());
+
+        return $helloCommand->run($helloInput, $output);
+    }
+
+    /**
+     * @param InputInterface $input
+     */
+    private function validate(InputInterface $input)
+    {
+    }
+}
